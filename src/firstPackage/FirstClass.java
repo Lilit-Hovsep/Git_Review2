bd96905dc8868cd70cfa04cb5669b55d77746ac5
Lilit-Hovsep <114306603+Lilit-Hovsep@users.noreply.github.com>
2023-01-07 14:32:25 -0500

    Update FirstClass.java

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,6 +3,7 @@ package firstPackage;
 public class FirstClass {
 
     public static void main(String[] args) {
+        // this is very good codes
         System.out.println("First class");
     }
 }
